5f25f00249b192f20b944bab14dbd5c397fd209b
trdxlucifer <singhdepu566@gmail.com>
2023-05-28 00:22:52 +0530

    added a menu and added immage and frmaer motion on text to mopvie from left

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -1,38 +1,40 @@
-import React from "react";
+
 import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
-const Landing = ({ setSelectedPag }) => {
-  const isAboveMediumScreen = useMediaQuery("(min-width:1060px)");
+const Landing = ({ setSelectedPage }) => {
+  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
   return (
     <section
       id="home"
-      className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10"
+      className="md:flex md:justify-between md:items-center gap-16 md:h-full py-10"
     >
-      {/* image  */}
-      <div className="md:order-2  justify-center basis-3/5 z-10 mt-16 md:mt-32">
-        {isAboveMediumScreen ? (
+      {/* IMAGE SECTION */}
+      <div className="basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
+        {isAboveLarge ? (
           <div
-            className="relative z-0 ml-20 before:absolute
-                     before:-top-20 before:-left-20 before:rounded-t-[400px] before:w-full befpre:max-w[400px] before:h-full
-                     before:border-2 before:border-blue before:z-[-1]"
+            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
+            before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
           >
             <img
-              src="src\assets\profile-image.png"
               alt="profile"
               className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+              src="src\assets\profile-image.png"
             />
           </div>
         ) : (
           <img
-            src="src\assets\profile-image.png"
             alt="profile"
-            className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+            className="z-10 w-full max-w-[400px] md:max-w-[600px]"
+            src="src\assets\profile-image.png"
           />
         )}
       </div>
-      <div className="z-30 basis-2/3 mt-12 md:mtt-32">
+
+      {/* MAIN TEXT */}
+      <div className="z-30 basis-2/5 mt-12 md:mt-32">
+        {/* HEADINGS */}
         <motion.div
           initial="hidden"
           whileInView="visible"
@@ -44,19 +46,64 @@ const Landing = ({ setSelectedPag }) => {
           }}
         >
           <p className="text-6xl font-playfair z-10 text-center md:text-start">
-            Deependra{""}
+            Deependra {""}
             <span
               className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush
-                    before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]"
+              before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]"
             >
-              Singh
+              singh
             </span>
           </p>
-          <p className="mt-10 mb-7 text-sm text-center ">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo odio
-            voluptate sunt eveniet ratione repudiandae porro temporibus quia
-            praesentium earum.
+
+          <p className="mt-10 mb-7 text-sm text-center md:text-start">
+            Adipiscing arcu, in aliquam fringilla cursus. Elit arcu elementum
+            viverra malesuada sem ac faucibus dolor. Sagittis scelerisque.
           </p>
+        </motion.div>
+
+        {/* CALL TO ACTIONS */}
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <AnchorLink
+            className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold
+              hover:bg-blue hover:text-white transition duration-500"
+            onClick={() => setSelectedPage("contact")}
+            href="#contact"
+          >
+            Contact Me
+          </AnchorLink>
+          <AnchorLink
+            className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
+            onClick={() => setSelectedPage("contact")}
+            href="#contact"
+          >
+            <div className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center justify-center px-10 font-playfair">
+              Let's talk.
+            </div>
+          </AnchorLink>
+        </motion.div>
+
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+        
         </motion.div>
       </div>
     </section>
